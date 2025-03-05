f96d8bbaf044f6f0b7c37eadc32612fa254bec95
barnabasd <ubarnabas.dev@gmail.com>
2025-03-05 19:55:45 +0100

    Backup

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const wrappers = document.querySelectorAll(".magic");
 
-const slidingimg_wrapper = document.getElementById("slidingimg_wrapper");
-const slidingimg = document.getElementById("slidingimg");
+function recalc() {
+    wrappers.forEach(wrapper => {
+        const scrollDivider = parseInt(wrapper.getAttribute("data-scroll-divider"));
+        const defaultmargindata = wrapper.getAttribute("data-pos").split("-");
+        wrapper.style.marginTop = `calc(${parseInt(defaultmargindata[1])}% + ${scrollY / scrollDivider}px)`;
+        wrapper.style.marginLeft = defaultmargindata[0] + "%";
+        const rotateDivider = parseInt(wrapper.getAttribute("data-rotate-divider"));
+        const rotatemod = parseInt(wrapper.getAttribute("data-rotate-add"));
+        const positive = wrapper.getAttribute("data-rotate-positive") == "true" ? "" : "-";
+        const rotate = (scrollY / rotateDivider) + rotatemod;
+        wrapper.style.transform = `rotate(${positive}${rotate}deg)`;
+    });
+}
 
-document.addEventListener('scroll', () => {
-    slidingimg_wrapper.style.marginTop = ((scrollY / 2)) + "px";
-    slidingimg.style.transform = "rotate(-" + (scrollY / 24) + "deg)"
-});
+document.addEventListener('scroll', recalc);
+recalc();
